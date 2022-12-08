37fa8ea38ac9f9eff834eb59c176c5fa4223251a
markussigurbjorns <markussigurbjorns@gmail.com>
2022-12-08 01:03:43 +0100

    5

--- day5/day5.py
+++ day5/day5.py
@@ -1,9 +1,23 @@
 import re
+import numpy as np
 
 f = open("input.txt", "r")
 
-for x in f:
-    numbers = re.findall(r'\w+',x)
-    print(numbers)
+readData = 0
 
+data =[]
+
+for x in f:
+    if readData < 8:
+        data.append([x[1],x[5],x[9],x[13],x[17],x[21],x[25],x[29],x[33]])
+        readData =readData + 1
+    else:
+        break
+
+data_t = [list(x) for x in zip(*data)]
+print(data_t)
+
+for x in f:
+    numbers = re.findall(r'\d+',x)
+    print(numbers)
 f.close()
